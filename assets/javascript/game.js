8611dbdfaf953f17900f792468b033b35723f066
Max Lettenberger <maxlettenberger@gmail.com>
2018-09-26 18:54:32 -0500

    Add half html layout, image and gif files

---
+++ assets/javascript/game.js
@@ -0,0 +1,2 @@
+var leonardoGifSrc = "https://media1.tenor.com/images/8c8f18bec7ba6a1b7ddc2ef76664e9ae/tenor.gif?itemid=10584134";
+var loganGifSrc = "https://thumbs.gfycat.com/EssentialRegalCockroach-small.gif";
