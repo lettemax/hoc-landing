18415f2d49413e0cfbcc5f76e7d7d07e9b7e9268
Max Lettenberger <maxlettenberger@gmail.com>
2018-09-26 21:25:50 -0500

    Add gif files and questions array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,16 @@
+// Strings for gif files that were too big to store locally
 var leonardoGifSrc = "https://media1.tenor.com/images/8c8f18bec7ba6a1b7ddc2ef76664e9ae/tenor.gif?itemid=10584134";
 var loganGifSrc = "https://thumbs.gfycat.com/EssentialRegalCockroach-small.gif";
+
+// Array of all questions
+var questions = ["Who played Jordan Belfort in Wolf of Wall Street?", 
+                "Who had a lead role in the show Friends?",
+                "Who led the bad*ss pack of wolves in Ocean's Eight?",
+                "Who has a son named Jaden and a daughter named Willow?",
+                "Who plays the lead role in the movie Hitch?",
+                "Who co-stars in La La Land alongside the mysterious Ryan Gosling?",
+                "Who plays Brennan Huff in the movie, Step Brothers?",
+                "Whose name rhymes with Shmim Sharrey?",
+                "Who stars in the popular Netflix series, Dear White People?",
+                "Who co-stars alongside Bradley Cooper in Silver Linings Playbook?"];
+
